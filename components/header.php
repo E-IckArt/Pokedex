d3f336090e0fd75fce71cca45d59e10c4f24e7e3
Emilie Ickart <emi.ickart@gmail.com>
2022-06-14 17:29:55 +0200

    Ft-Add Header with navBar

---
+++ components/header.php
@@ -0,0 +1,26 @@
+<header class="text-primary">
+    <nav class="navbar navbar-expand-lg bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand text-primary" href="#">
+                <img src="./assets/img/logo.png" alt="Logo Pokeball" width="30" height="30" class="d-inline-block align-text-top">
+                Pokedex
+            </a>
+            <button class="navbar-toggler border-primary" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon "><img src="./assets//img/navbar_toggle_button.svg" alt=""></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                    <a class="nav-link active text-primary" aria-current="page" href="index.php">Accueil</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link text-primary" href="#">Types</a>
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Chercher" aria-label="Barre de recherche">
+                    <button class="btn btn-outline-primary" type="submit">Chercher</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+</header>
