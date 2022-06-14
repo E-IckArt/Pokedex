6c6511956a22d7fe87e8b6995ada094cf380862e
Emilie Ickart <emi.ickart@gmail.com>
2022-06-14 17:30:06 +0200

    Ft- Add footer

---
+++ components/footer.php
@@ -0,0 +1,17 @@
+<footer class="mt-auto">
+    <nav class="navbar navbar-expand-lg bg-dark">
+        <div class="container-fluid">
+            <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+                <li class="nav-item me-3">
+                    <a class="nav-link active text-primary" aria-current="page" href="index.php">Me contacter</a>
+                </li>
+                <li class="nav-item me-3">
+                    <a class="nav-link text-primary" href="#">Voir mes autres projets</a>
+                </li>
+                <li class="nav-item me-3">
+                    <a class="nav-link text-primary" href="#">Mentions légales</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</footer>
