fb73cadd58a48c1988fe8f289d306f68aa12fe14
Emilie Ickart <emi.ickart@gmail.com>
2022-06-22 10:55:42 +0200

    Ft-Add header and footer to index.php

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
+<?php
+include_once('controller/PokemonManager.php');
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,8 +13,9 @@
     <!-- Bootstrap CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 </head>
+
 <body class="d-flex flex-column min-vh-100">
-    <?php  include_once './components/header.php' ?>
+    <?php include_once './components/header.php' ?>
     <main class="container">
         <div class="row">
             <div class="col mt-5">
@@ -28,4 +34,5 @@
     <!-- Bootstrap JavaScript Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 </body>
+
 </html>
