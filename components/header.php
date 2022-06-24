2627a0e304d37ca1d493bcee0cfc32c7f0f2afcb
Emilie Ickart <emi.ickart@gmail.com>
2022-06-24 10:11:37 +0200

    Bf-Add header href link to create.php

--- components/header.php
+++ components/header.php
@@ -17,7 +17,7 @@
                         <a class="nav-link text-primary" href="#">Types</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link text-primary" href="#">Ajouter un pokémon</a>
+                        <a class="nav-link text-primary" href="create.php">Ajouter un pokémon</a>
                     </li>
                 </ul>
                 <form class="d-flex" role="search">
