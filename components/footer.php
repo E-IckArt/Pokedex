802620b7a1e5f8c3eb68e27d821e3f0b56ff1c10
Emilie Ickart <emi.ickart@gmail.com>
2022-06-24 09:57:17 +0200

    Ft-Add modal with lorem content for legal notices

--- components/footer.php
+++ components/footer.php
@@ -9,9 +9,78 @@
                     <a class="nav-link text-primary" href="https://github.com/E-IckArt">Voir mes autres projets</a>
                 </li>
                 <li class="nav-item me-3">
-                    <a class="nav-link text-primary" href="#">Mentions légales</a>
+                    <!-- Link trigger modal -->
+                    <a class="nav-link text-primary" href="#" role="button" data-bs-toggle="modal" data-bs-target="#LegalNotices">Mentions légales</a>
                 </li>
             </ul>
         </div>
     </nav>
 </footer>
+<!-- Footer's Modal-->
+<div class="modal fade" id="LegalNotices" tabindex="-1" aria-labelledby="LegalNoticesLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+        <div class="modal-content bg-light">
+            <div class="modal-header bg-dark text-primary">
+                <h5 class="modal-title" id="LegalNoticesLabel">
+                    Mentions Légales
+                </h5>
+                <button type="button" class="btn-close btn-outline-primary" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body px-5">
+                <p>
+                    <span>Lorem ipsum dolor sit amet consectetur
+                        adipisicing elit. Labore error ut consectetur
+                        quasi cupiditate impedit odit earum deserunt?
+                        Ducimus quam nemo iste autem modi reprehenderit
+                        sit saepe, doloremque doloribus
+                        necessitatibus!</span><span>Sequi iusto omnis doloribus laudantium, optio,
+                        laboriosam nam perferendis repudiandae aliquid
+                        reprehenderit odit dolorem eius obcaecati fugiat
+                        at. Autem aut possimus vitae dignissimos
+                        voluptatum itaque a temporibus cum repellendus
+                        commodi.</span><span>Voluptatem vel aut earum minima? Pariatur
+                        expedita nostrum incidunt eveniet eligendi, hic
+                        consequatur quam, nobis maiores nihil sapiente!
+                        Explicabo autem omnis officia possimus, eos et
+                        quas aperiam ab tempore totam!</span>
+                </p>
+                <p>
+                    <span>Lorem ipsum dolor sit amet consectetur
+                        adipisicing elit. Ad libero voluptate provident
+                        hic tempore omnis similique est cupiditate.
+                        Ducimus eaque placeat laborum? Consequatur
+                        commodi eos minus illum, itaque sequi
+                        nemo?</span><span>Qui sit nostrum amet atque quasi ut facilis
+                        accusamus repellendus! Optio unde vel magnam
+                        corrupti enim tempora a rerum fugit alias sit,
+                        distinctio iusto tempore necessitatibus autem,
+                        id vero ipsum.</span><span>Doloremque at sit facere! Accusamus voluptate
+                        non, inventore asperiores voluptatum fuga nobis
+                        placeat eveniet. Voluptatum soluta ex ducimus
+                        sequi, mollitia vero quo. Aut delectus fugiat
+                        sequi, modi ratione ullam quidem.</span>
+                </p>
+                <p>
+                    <span>Lorem ipsum dolor sit amet consectetur
+                        adipisicing elit. Nobis aliquam reprehenderit
+                        odio facilis in deserunt quos ipsa nisi placeat
+                        autem animi, ex suscipit corrupti? Numquam animi
+                        hic quis ipsam atque.</span><span>Amet deserunt ab provident itaque ut a officia
+                        enim id natus quidem quisquam soluta architecto
+                        numquam omnis repudiandae, ipsa explicabo?
+                        Cupiditate inventore perspiciatis repellat,
+                        dolor odit labore cum reiciendis quia?</span><span>Ipsa adipisci possimus eum magni quas, illum a
+                        dicta nulla ab nostrum, pariatur enim quis
+                        consequatur exercitationem consequuntur autem
+                        eius illo corporis aliquid quidem. Corrupti
+                        repellat id magnam voluptates in?</span>
+                </p>
+            </div>
+            <div class="modal-footer bg-dark text-primary">
+                <button type="button" class="btn btn-outline-primary" data-bs-dismiss="modal">
+                    Fermer
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
