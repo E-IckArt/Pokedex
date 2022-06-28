068757da043065c1eb9c3d359f98a4dbd6b96b13
Emilie Ickart <emi.ickart@gmail.com>
2022-06-28 18:16:49 +0200

    Bf- Description Modal content

--- views/getAll.php
+++ views/getAll.php
@@ -1,42 +1,50 @@
 <?php
 foreach ($pokemons as $pokemon) : ?>
+    <?php
+    $imagesManager->get($pokemon->getImage());
+    // $typesManager->get($pokemon->getType1());
+    ?>
     <div class="card mt-5 mx-auto bg-dark text-info shadow" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="image de <?= $pokemon->getName() ?>">
+        <img src="<?= $imagesManager
+                        ->get($pokemon->getImage())
+                        ->getPath() ?>" class="card-img-top w-auto h-25 p-2 mx-auto mt-3 border border-white rounded-3" alt="image de <?= $pokemon->getName() ?>">
+        <h5 class="card-title mt-3 text-center">
+            #<?= $pokemon->getNumber() ?> - <?= $pokemon->getName() ?></h5>
         <div class="card-body px-4">
-            <h5 class="card-title">
-                <?= $pokemon->getNumber()  // BUGFIX- Revoir le code car l'appel ne fonctionne pas : Number = NULL
-                ?># <?= $pokemon->getName() ?></h5>
             <div class="card-text">
                 <p><?= substr($pokemon->getDescription(), 0, 210) ?> ...</p>
-                <p><?= $pokemon->getType1() // BUGFIX- Renvoyer le nom et pas l'id 
+                <p><?= $pokemon->getType1()
+                    // BUGFIX- Renvoyer le nom et pas l'id 
                     ?> ...</p>
                 <p><?= $pokemon->getType2() // // BUGFIX- Renvoyer le nom et pas l'id
                     ?> ...</p>
             </div>
             <div class="d-flex flex-wrap justify-content-evenly">
                 <!-- Button trigger modal -->
-                <button type="button" class="btn btn-info mx-auto my-3" data-bs-toggle="modal" data-bs-target="#descriptionModal">
+                <button type="button" class="btn btn-info mx-auto my-3" data-bs-toggle="modal" data-bs-target="#descriptionModal<?= $pokemon->getName() ?>">
                     Voir la description complète
                 </button>
-                <a href="#" class="btn btn-warning">Modifier</a>
-                <a href="#" class="btn btn-danger">Supprimer</a>
+                <a href="update.php?id=<?= $pokemon->getId() ?>" class="btn btn-warning">Modifier</a>
+                <a href="delete.php?id=<?= $pokemon->getId() ?>" class="btn btn-danger">Supprimer</a>
             </div>
         </div>
-        <!-- Description Modal -->
-        <div class="modal fade " id="descriptionModal" tabindex="-1" aria-labelledby="descriptionModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="descriptionModalLabel"><?= $pokemon->getName() ?></h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body mx-auto px-5">
-                        <?= $pokemon->getDescription() // BUGFIX- Description de bulbizarre sur toutes les modales
-                        ?>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
-                    </div>
+    </div>
+<?php endforeach ?>
+<!-- Description Modal -->
+<?php foreach ($pokemons as $pokemon) : ?>
+    <div class="modal fade " id="descriptionModal<?= $pokemon->getName() ?>" tabindex="-1" aria-labelledby="descriptionModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header bg-dark text-info">
+                    <h5 class="modal-title" id="descriptionModalLabel"><?= $pokemon->getName() ?></h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body bg-light mx-auto px-5">
+                    <?= $pokemon->getDescription() // BUGFIX- Description de bulbizarre sur toutes les modales
+                    ?>
+                </div>
+                <div class="modal-footer bg-dark text-info">
+                    <button type="button" class="btn btn-outline-info" data-bs-dismiss="modal">Fermer</button>
                 </div>
             </div>
         </div>
