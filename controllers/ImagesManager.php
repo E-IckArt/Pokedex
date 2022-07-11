0c39166e06e21c41bcc46fe3fddf156d1022eb22
Emilie Ickart <emi.ickart@gmail.com>
2022-07-11 15:28:53 +0200

    Add db.ini file

--- controllers/ImagesManager.php
+++ controllers/ImagesManager.php
@@ -8,15 +8,26 @@ class ImagesManager
 
     public function __construct()
     {
+        $dbConfig = parse_ini_file('db.ini');
+
+        if ($dbConfig === false) {
+            echo 'Erreur : problème de configuration.';
+            exit;
+        }
+
         try {
-            $this->db = new PDO('mysql:host=localhost;dbname=pokedex;port=3306', 'root', '');
+            $this->db = new PDO(
+                $dbConfig['DB_DSN'],
+                $dbConfig['DB_USER'],
+                $dbConfig['DB_PASSWORD']
+            );
         } catch (PDOException $exception) {
             echo $exception->getMessage();
             // En cas d'erreur :
             // On envoie le message d'erreur dans un fichier .log (création automatique s'il n'existe pas)
             file_put_contents('dblogs.log', $exception->getMessage() . PHP_EOL, FILE_APPEND);
             // On affiche un message à l'utilisateur et on arrête tout
-            die('Erreur : Impossible de se connecter à la base de données');
+            exit('Erreur : Impossible de se connecter à la base de données');
         }
     }
 
